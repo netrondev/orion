18c46da9f07c774864e9909954e40efe4d9a0c9c
Rouan van der Ende <rouan@netron.co.za>
2025-06-13 03:30:28 +0200

    gizmo axis letters

--- src/gizmo.rs
+++ src/gizmo.rs
@@ -4,6 +4,7 @@ use bevy_simple_subsecond_system::hot;
 use bevy_text_mesh::{
     SizeUnit, TextMesh, TextMeshBundle, TextMeshFont, TextMeshSize, TextMeshStyle,
 };
+use eframe::epaint::text;
 
 #[derive(Component)]
 pub struct GizmoArm;
@@ -16,16 +17,12 @@ pub fn create_gizmo(
     prev_setup: Query<Entity, With<GizmoArm>>,
     asset_server: Res<AssetServer>,
 ) {
-    // for entity in prev_setup.iter() {
-    //     commands.entity(entity).despawn();
-    // }
-
     hot_reload_system(&mut commands, prev_setup);
 
-    let font: Handle<TextMeshFont> = asset_server.load("fonts/FiraSans-Medium.ttf");
-
-    let longarm = 3.0;
+    let longarm = 2.0;
     let diam = 0.05;
+    let text_offset = 0.4;
+    let font: Handle<TextMeshFont> = asset_server.load("fonts/FiraSans-Medium.ttf");
 
     commands.spawn((
         Mesh3d(meshes.add(Cuboid::new(diam * 5.0, diam * 5.0, diam * 5.0))),
@@ -50,11 +47,11 @@ pub fn create_gizmo(
     commands.spawn((
         TextMeshBundle {
             text_mesh: TextMesh {
-                text: String::from("Xasndjk"),
+                text: String::from("X"),
                 style: TextMeshStyle {
                     font: font.clone(),
-                    font_size: SizeUnit::NonStandard(10.),
-                    color: Color::srgb(0.0, 0.5, 0.0),
+                    font_size: SizeUnit::NonStandard(50.),
+                    color: x_color,
                     ..Default::default()
                 },
                 size: TextMeshSize {
@@ -62,18 +59,18 @@ pub fn create_gizmo(
                 },
                 ..Default::default()
             },
-
-            transform: Transform::from_xyz(longarm, 0.0, 0.0)
-                .looking_at(Vec3::new(0.1, -1.0, 0.0), -Vec3::X),
+            transform: Transform::from_translation(Vec3::new(longarm, 0.0, -text_offset))
+                .looking_at(Vec3::new(999.0, -1.0, -text_offset), Vec3::X),
             ..Default::default()
         },
         GizmoArm,
     ));
 
     // Y GREEN
+    let color_y = Color::from(tailwind::EMERALD_500);
     commands.spawn((
         Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        MeshMaterial3d(materials.add(Color::from(tailwind::EMERALD_500))),
+        MeshMaterial3d(materials.add(color_y)),
         Transform::from_scale(Vec3::new(diam, longarm, diam)).with_translation(Vec3::new(
             0.0,
             longarm / 2.0,
@@ -81,16 +78,60 @@ pub fn create_gizmo(
         )),
         GizmoArm,
     ));
+    commands.spawn((
+        TextMeshBundle {
+            text_mesh: TextMesh {
+                text: String::from("Y"),
+                style: TextMeshStyle {
+                    font: font.clone(),
+                    font_size: SizeUnit::NonStandard(50.),
+                    color: color_y,
+                    ..Default::default()
+                },
+                size: TextMeshSize {
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            transform: Transform::from_translation(Vec3::new(0.0, longarm, -text_offset))
+                .looking_at(Vec3::new(999.0, 0.0, -text_offset), Vec3::X),
+            ..Default::default()
+        },
+        GizmoArm,
+    ));
 
     // Z BLUE
+    let color_z = Color::from(tailwind::SKY_500);
     commands.spawn((
         Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        MeshMaterial3d(materials.add(Color::from(tailwind::SKY_500))),
+        MeshMaterial3d(materials.add(color_z)),
         Transform::from_scale(Vec3::new(diam, diam, longarm)).with_translation(Vec3::new(
             0.0,
             0.0,
             longarm / 2.0,
         )),
+        GizmoArm,
+    ));
+
+    commands.spawn((
+        TextMeshBundle {
+            text_mesh: TextMesh {
+                text: String::from("Z"),
+                style: TextMeshStyle {
+                    font: font.clone(),
+                    font_size: SizeUnit::NonStandard(50.),
+                    color: color_z,
+                    ..Default::default()
+                },
+                size: TextMeshSize {
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            transform: Transform::from_translation(Vec3::new(0.0, text_offset * 1.5, longarm))
+                .looking_at(Vec3::new(-999.0, 0.0, 0.0), -Vec3::Y),
+            ..Default::default()
+        },
         GizmoArm,
     ));
 }
